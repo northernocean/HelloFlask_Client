ce3d7bac543e42cb7784d95b605a55ee94d483a1
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-11-14 16:30:37 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import dbaccess as db
+
+load_dotenv()
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    xs, ys = db.get_earthquake_count_by_years()
+    return render_template('index.html', data={'xs': xs, 'ys': ys, 'data_source': db.DATA_SOURCE})
+
+if __name__ == "__main__":
+    app.run()
+
+
