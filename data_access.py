12f32f4eca91c49cb23a6f387144e72ae62c7789
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-12-20 22:45:49 -0500

    add static resources

---
+++ data_access.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+DATA_SOURCE = "hidden stream api"
+base_url = "https://hidden-stream-21468.herokuapp.com/"
+
+def get_earthquake_count_by_years():
+    xs = []
+    ys = []
+    resource = "api/earthquakes"
+    res = requests.get(base_url + resource)
+    if res.status_code == 200:
+        dict_temp = res.json()
+        xs = dict_temp["xs"]
+        ys = dict_temp["ys"]
+
+    return xs, ys
